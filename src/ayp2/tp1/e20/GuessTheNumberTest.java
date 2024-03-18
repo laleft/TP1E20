0557a8f48da6936e3647fc84c768d1a041986979
Santiago De Bernardez <sdebernardez@gmail.com>
2024-03-18 14:21:09 -0300

    Add comments

--- src/ayp2/tp1/e20/GuessTheNumberTest.java
+++ src/ayp2/tp1/e20/GuessTheNumberTest.java
@@ -1,5 +1,11 @@
 package ayp2.tp1.e20;
 
+/**
+ * Clase para probar la funcionalidad 
+ * de GuessTheNumber
+ * 
+ * @author sdebernardez
+ */
 public class GuessTheNumberTest {
 
 	public static void main(String[] args) {
